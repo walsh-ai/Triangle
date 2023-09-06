1339c1094bf28a13d1df59d158821411f6632390
csnirs <walsh.jthomas@outlook.com>
2023-09-06 22:38:19 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ Console.WriteLine($"Test array [10, 50, 5, 1] is triangular: {IsTriangular(arr_b
 
 public static partial class Program {
     /// <summary>
+    /// Solution to determining if a triplet (P, Q, R) satisfying 
+    /// 0 <= P < Q < R < N whereby A[P], A[Q], A[R] is triangular 
+    /// exists in array A of N integers. 
+    /// Triangular means that the sum of any two elements of the triplet
+    /// is larger than the value of the third element. 
+    /// This solution sorts the array ascending and then moves a sliding 
+    /// window over the elements from index i to i+2
+    /// When a[i] + a[i + 1] > a[i + 2] the triangular condition is satisfied 
+    /// as a[i], a[i+1] are the smallest elements of the triplet 
     /// </summary>
     /// <param name="arr"></param>
     /// <returns></returns>
